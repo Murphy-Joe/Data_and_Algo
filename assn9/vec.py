1ce7755f9bb4d277ba32197ab10fa4358333aa19
Joe Murphy <ibidnsell@yahoo.com>
2021-04-29 10:30:59 -0400

    created folder for this mess

---
+++ assn9/vec.py
@@ -0,0 +1,19 @@
+class Vertex:
+    def __init__(self, number):
+        self.number = number
+        self.neighbors = []
+        self.edges = []
+
+class Edge: 
+    def __init__(self, vert1, vert2):
+        self.edge = (vert1, vert2)
+
+class Graph:
+    def __init__(self, vertices):
+        self.vertices = vertices
+
+    def search_breadth():
+        pass
+
+    def search_depth():
+        pass
